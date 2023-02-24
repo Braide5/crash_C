5b6abb5becfb15fff86ec94a2c9262d35b01c371
Katerina Braide <amkaterinabraide@gmail.com>
2023-02-24 21:30:15 +0100

    ...more progress

--- P_G.go
+++ P_G.go
@@ -3,17 +3,10 @@ package main
 import (
 	"log"
 	"math/rand"
-	//"fmt"
-
-	
 )
-
 func main() {
 	// Generate a password that is 64 characters long with 10 digits, 10 symbols,
 	// allowing upper and lower casae letters, disallowing repeat characyers.
 	res := rand.Intn(2000000000)
-	
 	log.Printf("%U", res)
-	//fmt.Println(res)
-	//fmt.Println(rand.Intn(2000000000))
 }
