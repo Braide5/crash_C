d1f00b8fbbc7e82234ef6d025639b2bf9df4fe83
Katerina Braide <amkaterinabraide@gmail.com>
2023-02-25 20:27:58 +0100

    F done #lol

--- R_N.go
+++ R_N.go
@@ -1,54 +1,17 @@
-// Generating Random Numbers in Golang
 package main
 
 import (
-	"fmt"
-	"math/rand"
-	"time"
+    "fmt"
+    "math/rand"
+    "time"
 )
 
 func main() {
+    // Set the seed value for the random number generator
+    rand.Seed(time.Now().UnixNano())
 
-
-	fmt.Println(rand.Intn(200))
-	fmt.Println(rand.Intn(200))
-	fmt.Println(rand.Intn(200))
-	fmt.Println()
-
-	
-	fmt.Println(rand.Float64())
-
-	// By default, it uses the value 1.
-	fmt.Println((rand.Float64() * 8) + 7)
-	fmt.Println((rand.Float64() * 8) + 7)
-	fmt.Println()
-
-	// Seeding - Go provides a method,
-	// Seed(see int64), that allows you
-	// to initialize this default sequence.
-	
-	// Implementation is slow
-	// to make it faster
-	// rand.Seed(time.Now().UnixNano())
-	// is added. Seed is the current time,
-	// converted to int64 by UnixNano.
-	// Gives constantly changing numbers
-	x1 := rand.NewSource(time.Now().UnixNano())
-	y1 := rand.New(x1)
-	
-	fmt.Println(y1.Intn(200))
-	fmt.Println(y1.Intn(200))
-	fmt.Println()
-
-	x2 := rand.NewSource(55)
-	y2 := rand.New(x2)
-	fmt.Println(y2.Intn(200))
-	fmt.Println(y2.Intn(200))
-	fmt.Println()
-	
-	x3 := rand.NewSource(5)
-	y3 := rand.New(x3)
-	fmt.Println(y3.Intn(200))
-	fmt.Println(y3.Intn(200))
+    // Generate and print 10 random integers between 0 and 99
+    for i := 0; i < 10; i++ {
+        fmt.Println(rand.Intn(100))
+    }
 }
-// last output is rigid, fix it!
