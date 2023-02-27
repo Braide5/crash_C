f79bda41b77f613c5c4bc97f54f66dc8d0e38199
Katerina Braide <amkaterinabraide@gmail.com>
2023-02-27 19:52:10 +0100

    finally D

--- Time.go
+++ Time.go
@@ -8,12 +8,11 @@ import (
 func main() {
     // Get the current time
     t := time.Now()
-    now := time.Now()
-
+  
     // Get the day of the week
-    dayOfWeek := now.Weekday()
+    dayOfWeek := t.Weekday()
 
     // Print the day of the week
     fmt.Printf("Today is %s\n", dayOfWeek)
-	fmt.Printf("the time is %d", t.Format("15:04 PM"))
+	fmt.Printf("the time is %s", t.Format("15:04 PM"))
 }
