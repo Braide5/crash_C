1bc5bfbf3b6dfb7619233c0d5aedec22ff93179b
Katerina Braide <amkaterinabraide@gmail.com>
2023-02-27 19:45:57 +0100

    fix time 12f

--- Time.go
+++ Time.go
@@ -7,6 +7,7 @@ import (
 
 func main() {
     // Get the current time
+    t := time.Now()
     now := time.Now()
 
     // Get the day of the week
@@ -14,5 +15,5 @@ func main() {
 
     // Print the day of the week
     fmt.Printf("Today is %s\n", dayOfWeek)
-	fmt.Printf("the time is %d", now)
+	fmt.Printf("the time is %d", t.Format("15:04 PM"))
 }
